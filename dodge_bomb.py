6518f5813ad80dc3f9efeebf853bd0b46e9ed715
c0a25104f2 <c0a25104f2@edu.teu.ac.jp>
2026-06-16 14:52:57 +0900

    練習４：当たり判定作成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -53,6 +53,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct):  #こうかとRectと爆弾Rectが重なったら
+            print("ゲームオーバー")
+            return
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
